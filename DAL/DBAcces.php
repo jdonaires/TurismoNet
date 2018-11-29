ebc36909011913377b5abc998d7927f83966fa3f
Jose Herrera <44219730+JoseHCas@users.noreply.github.com>
2018-11-28 23:10:04 -0500

    ORDENAMIENTO DE ARCHIVOS
    ORDENAMIENTO DE ARCHIVOS PARA COMENZAR A AVANZAR...

---
+++ DAL/DBAcces.php
@@ -0,0 +1,20 @@
+<?php
+class DBAccess
+{
+  private $conn;
+  public function __CONSTRUCT()
+  {
+    try {
+      $this->conn = new PDO('mysql:host=localhost;dbname=Turismonet', 'root', '12345');
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e ) {
+      echo "error:" .$e->getMessage();
+    }
+ }
+
+  public function get_connection()
+  {
+    return $this->conn;
+  }
+}
+ ?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ICA | Turismo Net</title>
+    <link rel="stylesheet" href="DESIGNER/CSS/index.css">
+  </head>
+  <body>
+    <div class="contenedor-fondo">
+      <!-- <img src="DESIGNER/IMG/fondo_index.jpg"> -->
+      <div class="contenedor-menu">
+        <div class="contenedor-opciones">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Contactanos</a></li>
+          <li><a href="#">Servicios</a></li>
+          <li><a href="#">Login</a></li>
+        </div>
+        <div class="contenedor-redes">
+        </div>
+      </div>
+      <div class="contenedor-logo">
+        <img src="DESIGNER/IMG/logo.png">
+      </div>
+    </div>
+  </body>
+</html>
