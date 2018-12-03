03e8e1656296155b0b029295c502e6bf286fe5f8
Arrazabayimy <jimi_atom_15@hotmail.com>
2018-12-03 16:07:08 -0500

    Carpeta DAO

---
+++ DAO/Lugar_turistico.php
@@ -0,0 +1,66 @@
+<?php
+require_once('../DAL/DBAccess.php');
+require_once('../BOL/turistico.php');
+
+class TuristicoDAO
+{
+	private $pdo;
+
+	public function __CONSTRUCT()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+
+	public function Registrar_Turistico(Turistico $persona)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("CALL regLugarTuristico(?,?,?,?,?)");
+    $statement->bindParam(1,$persona->__GET('titulo'));
+		$statement->bindParam(2,$persona->__GET('descripcion'));
+		$statement->bindParam(3,$persona->__GET('imgLugar'));
+  	$statement->bindParam(4,$persona->__GET('idUbicacion'));
+		$statement->bindParam(5,$persona->__GET('fecha'));
+    $statement -> execute();
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+	public function Listar_Turistico(Turistico $persona)
+	{
+		try
+		{
+			$result = array();
+
+			$statement = $this->pdo->prepare("call sp_buscar_lugarturistico(?)");
+			$statement->bindParam(1,$persona->__GET('idLugar'));
+			$statement->execute();
+
+			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+			{
+				$per = new turistico();
+
+				$per->__SET('idLugar', $r->idLugar);
+				$per->__SET('titulo', $r->titulo);
+				$per->__SET('descripcion', $r->descripcion);
+				$per->__SET('imgLugar', $r->imgLugar);
+				$per->__SET('idUbicacion', $r->idUbicacion);
+				$per->__SET('fecha', $r->fecha);
+
+				$result[] = $per;
+			}
+
+			return $result;
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+}
+
+?>
