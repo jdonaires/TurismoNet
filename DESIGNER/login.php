d3a6f4f882d17a1019ddf0460255c1eabe9d9982
Jose Herrera <44219730+JoseHCas@users.noreply.github.com>
2018-12-04 16:56:56 -0500

    SIN IMPORTANCIA

--- DESIGNER/login.php
+++ DESIGNER/login.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="CSS/login.css">
+    <link rel="stylesheet" href="CSS/style.css">
   </head>
   <body>
     <div class="contenedor-fondo">
@@ -10,11 +11,11 @@
         <form class="" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
           <h4>INICIAR SESIÓN</h4>
           <div class="contenedor-usuario-contraseña">
-            <li></li>
+            <li><span class="icono icon-user"></span></li>
             <input type="text" name="" value="" placeholder="Ingrese su Usuario" required>
           </div>
           <div class="contenedor-usuario-contraseña">
-            <li></li>
+            <li><span class="icono icon-key"></span></li>
             <input type="password" name="" value="" placeholder="Ingresu Contraseña" required>
           </div>
           <div class="contenedor-botones">
