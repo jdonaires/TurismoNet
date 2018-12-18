565c819bd189c441fcb581a6b440ef423b942c54
Jose Herrera <44219730+JoseHCas@users.noreply.github.com>
2018-12-18 09:17:20 -0500

    Update frm_bienvenido.php
    Co-Authored-By: branndonmr <branndonmr@users.noreply.github.com>

--- DESIGNER/frm_bienvenido.php
+++ DESIGNER/frm_bienvenido.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
   </head>
   <body>
-    <h1>Bievenido a</h1>
-    <h1>TurismoNet</h1>
+    <h1 style="font-size:35px; text-align:center; font-family: Arial;" >Bievenido a</h1>
+    <h1 style="font-size:35px; text-align:center; font-family: Arial;" >TurismoNet</h1>
   </body>
 </html>
